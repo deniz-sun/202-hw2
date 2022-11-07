c0de9498e50e161131cd70a14f6b8807c711ac3c
deniz-sun <70107128+deniz-sun@users.noreply.github.com>
2022-11-07 12:49:35 +0300

    Add files via upload

---
+++ analysis.cpp
@@ -0,0 +1,37 @@
+/**
+* Title : Binary Search Trees
+* Author : Deniz Sun
+* ID: 22003981
+* Section : 1
+* Assignment : 2
+* Description :
+ *
+ *
+*/
+
+#include "analysis.h"
+
+void analysis::heightAnalysis() {
+    cout << "Part f - Analysis of BST height - part 1\n" <<
+            "-----------------------------------------\n" <<
+            "Tree Size \t\t\tTree Height" <<
+            "-----------------------------------------" << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    cout << "Part f - Analysis of BST height - part 2\n" <<
+         "-----------------------------------------\n" <<
+         "Tree Size \t\t\tTree Height" <<
+         "-----------------------------------------" << endl;
+
+}
---
+++ analysis.h
@@ -0,0 +1,23 @@
+/**
+* Title : Binary Search Trees
+* Author : Deniz Sun
+* ID: 22003981
+* Section : 1
+* Assignment : 2
+* Description :
+ *
+ *
+*/
+
+#ifndef INC_202_HW2_ANALYSIS_H
+#define INC_202_HW2_ANALYSIS_H
+#include <iostream>
+using namespace std;
+
+class analysis {
+    void heightAnalysis();
+
+};
+
+
+#endif //INC_202_HW2_ANALYSIS_H
