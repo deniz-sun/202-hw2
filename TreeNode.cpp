3d394614adb4befc6a4322e3e477c7ff2c40e536
deniz-sun <70107128+deniz-sun@users.noreply.github.com>
2022-11-11 21:55:32 +0300

    Update TreeNode.cpp

--- TreeNode.cpp
+++ TreeNode.cpp
@@ -4,16 +4,16 @@
 * ID: 22003981
 * Section : 1
 * Assignment : 2
-* Description :
- *
- *
+* Description : The tree node for binary search tree
 */
 
 
 #include "TreeNode.h"
+//default constructor for tree nodes
 TreeNode::TreeNode() {
 
 }
+// constructor for tree nodes that initialize the data and the left, right children
 TreeNode::TreeNode(const int &data, TreeNode *left, TreeNode *right) {
     this->data = data;
     this->left = left;
