07027627b58c691d44deacc9e45e9d3774cfa771
deniz-sun <70107128+deniz-sun@users.noreply.github.com>
2022-11-11 21:54:18 +0300

    Update analysis.cpp

--- analysis.cpp
+++ analysis.cpp
@@ -4,34 +4,49 @@
 * ID: 22003981
 * Section : 1
 * Assignment : 2
-* Description :
- *
- *
+* Description : analysis class with the heightAnalysis function
 */
 
 #include "analysis.h"
+#include "BST.h"
+void heightAnalysis() {
 
-void analysis::heightAnalysis() {
-    cout << "Part f - Analysis of BST height - part 1\n" <<
+    BST tree;
+    int size = 30000;
+    int array[size];
+
+    //outputs the result of analysis
+    cout << "\nPart f - Analysis of BST height - part 1\n" <<
             "-----------------------------------------\n" <<
-            "Tree Size \t\t\tTree Height" <<
+            "Tree Size \tTree Height\n" <<
             "-----------------------------------------" << endl;
 
+    //create random integers and insert them into the tree
+    for (int i = 0; i < size; ++i) {
+        array[i] =  rand() % size + 1;
+        tree.insert(array[i]);
 
+        //print the node count and height at each 1500 insertion
+        if ((i + 1) % 1500 == 0)
+            cout << tree.getNodeCount() << "\t\t" << tree.getHeight() << endl;
+    }
+    //shuffle array by traversing and changing items at an index
+    int temp;
+    for (int i = size - 1; i > 0; i--) {
+        int random = rand() % i;
+        temp = array[i];
+        array[i] = array[random];
+        array[random] = temp;
+    }
 
-
-
-
-
-
-
-
-
-
-
-    cout << "Part f - Analysis of BST height - part 2\n" <<
+    //outputs the results after the array is shuffled
+    cout << "\nPart f - Analysis of BST height - part 2\n" <<
          "-----------------------------------------\n" <<
-         "Tree Size \t\t\tTree Height" <<
+         "Tree Size \tTree Height\n" <<
          "-----------------------------------------" << endl;
-
+    for (int i = 0; i < size; ++i) {
+        tree.deleteKey(array[i]);
+        if ((i + 1) % 1500 == 0)
+            cout << tree.getNodeCount() << "\t\t" << tree.getHeight() << endl;
+    }
 }
